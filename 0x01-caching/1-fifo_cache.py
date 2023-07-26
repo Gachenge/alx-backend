e351d18e5e58eb35247b45261eed3a0afbac378b
Gachenge <gachenge1@gmail.com>
2023-07-26 11:17:54 +0300

    add docstrings

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,28 +1,45 @@
 #!/usr/bin/env python3
+"""Create a class FIFOCache that inherits from
+BaseCaching and is a caching system:
 """
-class FIFOCache that inherits from BaseCaching and is a caching system
-"""
+
+
 BaseCaching = __import__('base_caching').BaseCaching
 
 
 class FIFOCache(BaseCaching):
-    """First in first out caching"""
+    """_summary_
+    """
+
     def __init__(self):
-        """initialise the data dictionary"""
+        """_summary_
+        """
         super().__init__()
 
     def put(self, key, item):
-        """assign to the dictionary the item to the key"""
-        if key is not None and item is not None:
+        """_summary_
+
+        Args:
+                        key (_type_): _description_
+                        item (_type_): _description_
+        """
+        if key is None or item is None:
+            pass
+        else:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS \
+                    and key not in self.cache_data.keys():
+                first_key = next(iter(self.cache_data.keys()))
+                del self.cache_data[first_key]
+                print("DISCARD: {}". format(first_key))
+
             self.cache_data[key] = item
-            items = len(self.cache_data.values())
-            if items > BaseCaching.MAX_ITEMS:
-                (k := next(iter(self.cache_data)), self.cache_data.pop(k))
-                print(f"DISCARD: {k}")
 
     def get(self, key):
-        """return the value in self.cache_data linked to key"""
+        """return the value in self.cache_data linked to key
+
+        Args:
+                        key (_type_): _description_
+        """
         if key is None or key not in self.cache_data.keys():
             return None
-        else:
-            return(self.cache_data.get(key))
+        return self.cache_data.get(key)
