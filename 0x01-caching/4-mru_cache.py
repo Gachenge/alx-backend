e7c61c0bd9e5993c0edabf6887c8208d1735c9a8
Gachenge <gachenge1@gmail.com>
2023-07-26 12:20:46 +0300

    corrections

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -11,27 +11,25 @@ class MRUCache(BaseCaching):
     def __init__(self):
         """initialise the class"""
         super().__init__()
-        self.lst = []
-
-    def get(self, key):
-        """return value in self.cache_data"""
-        self.lst.append(key)
-        if key is None or key not in self.cache_data.keys():
-            return None
-        else:
-            return(self.cache_data.get(key))
+        self.usedKeys = []
 
     def put(self, key, item):
-        """
-        Must assign to the dictionary self.cache_data the
-        item value for the key key.
-        """
+        """return value in self.cache_data"""
         if key is not None and item is not None:
             self.cache_data[key] = item
-            self.lst.append(key)
-            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-                ke = mode(self.lst)
-                if ke == 1:
-                    ke = self.lst[len(self.lst)]
-                self.cache_data.pop(ke)
-                print(f"DISCARD: {ke}")
+            if key not in self.usedKeys:
+                self.usedKeys.append(key)
+            else:
+                self.usedKeys.append(
+                    self.usedKeys.pop(self.usedKeys.index(key)))
+            if len(self.usedKeys) > BaseCaching.MAX_ITEMS:
+                discard = self.usedKeys.pop(-2)
+                del self.cache_data[discard]
+                print('DISCARD: {:s}'.format(discard))
+
+    def get(self, key):
+        """return the value in self.cache_data linked to key"""
+        if key is not None and key in self.cache_data.keys():
+            self.usedKeys.append(self.usedKeys.pop(self.usedKeys.index(key)))
+            return self.cache_data.get(key)
+        return None
